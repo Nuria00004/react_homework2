60e51b61f01199090226714079a9f64008cd69d6
Nuria00004 <nuriarustambekova4@gmail.com>
2023-09-20 13:46:43 +0600

    h2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 function App() {
+
+const [color1, setColor1] = useState('red')
+const [color2, setColor2] = useState('blue')
+  
+  function swapColors() {
+    setColor1(color2)
+    setColor2(color1)
+  }
+
   return (
     <div className="App">
+      <div style={{width:'200px', height:'200px', backgroundColor:color1}} onClick={swapColors}></div>
+      <div style={{width:'200px', height:'200px', backgroundColor:color2}} onClick={swapColors}></div>
+      
     </div>
   );
 }
